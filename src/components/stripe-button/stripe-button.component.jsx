016ff566387799f9a128d169042d7c91ccea6a2c
bgr7597 <bgr7597@hotmail.com>
2021-02-17 12:12:48 -0500

    Added stripe payment integration and replaced favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51ILrc3LalVIEXCU4Zz98c1GpxaJhugoNKwR3jUeC8GnyOIKHqQ5GYQbjZXnDOICOYVPndfjuVe420Q9T9OEnhVVe00aTRKwkQK';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
