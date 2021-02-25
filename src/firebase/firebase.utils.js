ab8abb528ab270483a92a1fdffbb7d12cbb1829f
framptonb <bgr7597@hotmail.com>
2021-02-24 21:07:20 -0400

    updated firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  apiKey: 'AIzaSyBIXJsXPU7fpIY6YXMZCCO4U8Sjoa2Gvz4',
   authDomain: 'crwn-db.firebaseapp.com',
   databaseURL: 'https://crwn-db.firebaseio.com',
   projectId: 'crwn-db',
